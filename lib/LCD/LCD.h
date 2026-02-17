82ae4a5bb969c6e5818e1373de22535df76a7c42
Jack <chaichanchaisak2549@gmail.com>
2026-02-17 23:43:09 +0700

    Fix parameter documentation formatting in LCD class

--- lib/LCD/LCD.h
+++ lib/LCD/LCD.h
@@ -11,9 +11,9 @@ class LCD {
 
   public:
     /**
-     * ? @param RES พินสำหรับ Reset ของ LCD
-     * ? @param DC พินสำหรับ Data/Command ของ LCD
-     * ? @param CS พินสำหรับ Chip Select ของ LCD
+     * @param RES พินสำหรับ Reset ของ LCD
+     * @param DC พินสำหรับ Data/Command ของ LCD
+     * @param CS พินสำหรับ Chip Select ของ LCD
      */
     LCD(int RES , int DC , int CS);
 
@@ -30,22 +30,22 @@ class LCD {
     void clearScreen();
 
     /**
-     * ? @param x ตำแหน่งแกน X ที่ต้องการตั้ง Cursor
-     * ? @param y ตำแหน่งแกน Y ที่ต้องการตั้ง Cursor
+     * @param x ตำแหน่งแกน X ที่ต้องการตั้ง Cursor
+     * @param y ตำแหน่งแกน Y ที่ต้องการตั้ง Cursor
      * ฟังก์ชันสำหรับตั้งตำแหน่ง Cursor บนหน้าจอ LCD เพื่อเตรียมพร้อมสำหรับการพิมพ์ข้อความในตำแหน่งนั้น
      */
     void setCursor(int x, int y);
 
     /**
-     * ? @param text ข้อความที่ต้องการพิมพ์บนหน้าจอ LCD
+     * @param text ข้อความที่ต้องการพิมพ์บนหน้าจอ LCD
      * ฟังก์ชันสำหรับพิมพ์ข้อความบนหน้าจอ LCD โดยข้อความจะถูกพิมพ์ที่ตำแหน่งที่ตั้งไว้ด้วย setCursor()
      */
     void print(const String &text);
 
     /**
-     * ? @param r ค่าความเข้มของสีแดง (0-255)
-     * ? @param g ค่าความเข้มของสีเขียว (0-255)
-     * ? @param b ค่าความเข้มของสีน้ำเงิน (0-255)
+     * @param r ค่าความเข้มของสีแดง (0-255)
+     * @param g ค่าความเข้มของสีเขียว (0-255)
+     * @param b ค่าความเข้มของสีน้ำเงิน (0-255)
      * ฟังก์ชันสำหรับตั้งค่าสีที่ใช้ในการพิมพ์ข้อความบนหน้าจอ LCD โดยรับค่าความเข้มของสีแดง เขียว และน้ำเงินเป็นพารามิเตอร์
      */
     void setColor(int r, int g, int b);
